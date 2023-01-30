e2062730d5d3274c93d90a389d4b0d74ded43c22
Taylor Remund <remundtaylor3@gmail.com>
2023-01-30 15:14:29 -0700

    init commit

---
+++ practice.py
@@ -0,0 +1,32 @@
+from database import add_entry, get_entries
+
+menu = '''
+Please Select one of the following options:
+1) Add new entry for today.
+2) View entries.
+3) Exit.
+
+Your selection: 
+'''
+print('Welcome to the programming diary!')
+
+def prompt_new_entry():
+  add_entry(input('What have you learned today? '), input('Enter the date: '))
+
+def view_entries(entries):
+  for entry in entries:
+      print(f"{entry['date']}\n{entry['content']}\n\n")
+
+# NON-WALRUS
+# while user_input != '3':
+#   user_input = input(menu)
+
+# WALRUS
+while (user_input := input(menu)) != '3':
+  if user_input == '1':
+    prompt_new_entry()
+  elif user_input == '2':
+    view_entries(get_entries())
+  else:
+    print('Invalid option, please try again!')
+
